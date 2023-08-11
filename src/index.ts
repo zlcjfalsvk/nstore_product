dba9b77949360419f3e6bb51b84249e2c527c2c4
철철 <zl0759vk@naver.com>
2023-08-11 17:33:10 +0900

    Develop/naver (#1)
    * naver 작업 백업
    
    * util 작성
    
    * naver 관련 작업 추가
    
    * gitignore 추가
    
    * package module 추가
    
    * naver 관련 작업

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,45 @@
-console.log('Hi');
+import * as readline from 'readline/promises';
+import { stdin as input, stdout as output } from 'process';
+
+import { Naver } from './scraper';
+import * as fs from 'fs';
+import { Parser } from '@json2csv/plainjs';
+
+const NAVER_SMART_STORE_DEFAULT_URL = 'https://smartstore.naver.com';
+
+let rl: readline.Interface;
+const start = async () => {
+	rl = readline.createInterface({ input, output });
+	const answer = await rl.question(
+		`네이버 스마트 스토어 사이트 입력 ex) ${NAVER_SMART_STORE_DEFAULT_URL}/dongsmarkett \r`,
+	);
+
+	console.log(`${answer} 상품 리스트를 추출 합니다`);
+
+	const naverScraper = new Naver(
+		NAVER_SMART_STORE_DEFAULT_URL,
+		answer.replace(NAVER_SMART_STORE_DEFAULT_URL + '/', ''),
+	);
+
+	const marketReadableName = await naverScraper.Start();
+	// CSV 추출
+	const jsonData = JSON.parse(
+		fs.readFileSync(`db/${marketReadableName}.json`).toString(),
+	);
+
+	const opts = {};
+	const parser = new Parser(opts);
+
+	const list = [];
+	for (const productId in jsonData) {
+		list.push(jsonData[productId]);
+	}
+	const parsedList = parser.parse(list);
+	fs.writeFileSync(`${marketReadableName}.csv`, parsedList);
+};
+
+start().then(async () => {
+	await rl.question(`종료하기 위해 아무키나 눌러주세요`);
+	rl.close();
+	process.exit();
+});
---
+++ src/scraper/naver/naver.ts
@@ -0,0 +1,162 @@
+import axios from 'axios';
+import { Product, ProductPage, ITransformProduct } from './index';
+import { Config, JsonDB } from 'node-json-db';
+import { Sleep } from '../../util';
+
+const tag = {
+	NEW: 'NEW',
+	BEST: 'BEST',
+} as const;
+
+export class Naver {
+	#defaultUrl: string;
+	#name: string;
+	#db: JsonDB = {} as JsonDB;
+	#channelUid: string = '';
+	#channelName: string = '';
+
+	bestProductNos: number[] = [];
+	newProductNos: number[] = [];
+
+	constructor(defaultUrl: string, name: string) {
+		this.#defaultUrl = defaultUrl;
+		this.#name = name;
+	}
+
+	async Start(): Promise<string> {
+		try {
+			await this.getMarketInfo();
+			await this.getProducts();
+
+			return this.#channelName;
+		} catch (e) {
+			console.error(e);
+		}
+
+		return '';
+	}
+
+	private async getMarketInfo(): Promise<void> {
+		const url = `${this.#defaultUrl}/i/v1/smart-stores?url=${this.#name}`;
+		try {
+			const responseData = (await axios.get(url))?.data;
+
+			this.#channelUid = responseData.channel.channelUid;
+			this.#channelName = responseData.channel.channelName;
+			this.#db = new JsonDB(
+				new Config(
+					`db/${responseData.channel.channelName}`,
+					true,
+					false,
+					'/',
+				),
+			);
+
+			this.bestProductNos =
+				responseData.specialProducts.bestProductNos || [];
+			this.newProductNos =
+				responseData.specialProducts.newProductNos || [];
+		} catch (e) {
+			throw new Error(`${this.#name}의 channelUid를 알 수 없습니다`);
+		}
+	}
+
+	private async getProducts(): Promise<void> {
+		let pageNum = 1;
+		const pageSize = 40;
+
+		const url = `${this.#defaultUrl}/i/v2/channels/${
+			this.#channelUid
+		}/categories/ALL/products?categorySearchType=STDCATG&sortType=TOTALSALE&page=${pageNum}&pageSize=${pageSize}`;
+
+		const headerReferer = `${this.#defaultUrl}/${
+			this.#name
+		}/category/ALL?st=TOTALSALE&dt=BIG_IMAGE&page=${pageNum}&size=${pageSize}`;
+
+		// 상품을 구하고 바로 가공을 한다
+		const firstPageData: ProductPage = (
+			await axios.get(url, {
+				headers: {
+					Referer: headerReferer,
+				},
+			})
+		)?.data;
+
+		await this.saveTransformDataOnFile(
+			this.transformData(firstPageData.simpleProducts),
+		);
+
+		// const totalPage = Math.ceil(pageSize / firstPageData.totalCount);
+		const totalPage = 2;
+		console.log(`${pageNum++}/${totalPage} 완료 ----------------`);
+		while (pageNum <= totalPage) {
+			const pageData: ProductPage = (
+				await axios.get(url, {
+					headers: {
+						Referer: headerReferer,
+					},
+				})
+			)?.data;
+
+			await this.saveTransformDataOnFile(
+				this.transformData(pageData.simpleProducts),
+			);
+
+			console.log(`${pageNum++}/${totalPage} 완료 ----------------`);
+			// 빠른 호출은 LateLimit 걸릴 수 있어 sleep 추가
+			await Sleep(500);
+		}
+	}
+
+	private transformData(data: Product[]): ITransformProduct[] {
+		const item: ITransformProduct[] = [];
+		for (const product of data) {
+			const newTransformProduct: ITransformProduct = {
+				id: product.id + '',
+				productNo: product.productNo + '',
+				name: product.name,
+				url: `${this.#defaultUrl}/${this.#name}/products/${product.id}`,
+				salePrice: product.salePrice,
+				discountedSalePrice:
+					product.benefitsView?.discountedSalePrice ||
+					product.salePrice,
+				mobileDiscountedSalePrice:
+					product.benefitsView?.mobileDiscountedSalePrice ||
+					product.salePrice,
+				totalReviewCount: product.reviewAmount?.totalReviewCount || 0,
+				tags: this.getTags(product.id),
+			};
+			item.push(newTransformProduct);
+		}
+
+		return item;
+	}
+
+	private async saveTransformDataOnFile(
+		data: ITransformProduct[],
+	): Promise<void> {
+		for (const transformProduct of data) {
+			const hasData = await this.#db.getObjectDefault(
+				`/${transformProduct.id}`,
+				{},
+			);
+			const newData = Object.assign({}, hasData, transformProduct);
+			await this.#db.push(`/${transformProduct.id}`, newData);
+			console.log(
+				`저장 완료 id: ${transformProduct.id}, productNo: ${transformProduct.productNo}, name: ${transformProduct.name} ---`,
+			);
+		}
+	}
+
+	private getTags(id: number): string[] {
+		const tags = [];
+		if (this.bestProductNos.includes(id)) {
+			tags.push(tag.BEST);
+		}
+		if (this.newProductNos.includes(id)) {
+			tags.push(tag.NEW);
+		}
+
+		return tags;
+	}
+}
